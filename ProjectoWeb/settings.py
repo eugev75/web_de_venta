45e893d31cc157e61de4a4de787f2e1a958803fe
eugev75 <122049425+eugev75@users.noreply.github.com>
2023-04-03 21:30:05 -0300

    setting

--- ProjectoWeb/settings.py
+++ ProjectoWeb/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
-
-ALLOWED_HOSTS = []
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:  
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+# DEBUG = 'RENDER' not in os.environ
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
+# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+# if RENDER_EXTERNAL_HOSTNAME:  
+#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 
